f0b95f93459f2e88965dec24ee4ed15033aba0cc
EliBrank <eliasbrinkman@gmail.com>
2024-07-31 13:47:50 -0500

    add html, js, css

---
+++ 7-31/script.js
@@ -0,0 +1,33 @@
+const apiKey = '277d9a366e11543e1f7521576e9b14c8';
+const genres = `https://api.themoviedb.org/3/genre/movie/list?language=en&api_key=${apiKey}`;
+const imgBaseUrl = 'http://image.tmdb.org/t/p/w342';
+
+function search () {
+  const genreId = $('#genres').val();
+  const genreSearch = `https://api.themoviedb.org/3/discover/movie?with_genres=${genreId}&api_key=${apiKey}`;
+  $.getJSON(genreSearch, function (data) {
+    $.each(data.results, function (i, film) {
+      const imgUrl = imgBaseUrl + film.poster_path;
+      let template = `
+        <div class='movie'>
+          <img class='lightbulb-effect' src='${imgUrl}'>
+          <h3>${film.original_title}</h3>
+          <p class='description'>${film.overview}</p>
+        </div>
+`;
+      $('#content').append(template);
+    });
+  });
+}
+
+$(document).ready(function () {
+  $.getJSON(genres, function (data) {
+    $.each(data.genres, function (i, genres) {
+      let opt = document.createElement('option');
+      opt.value = genres.id;
+      opt.innerText = genres.name;
+      $('select').append(opt);
+    });
+  });
+  $('button').on('click', search);
+});
